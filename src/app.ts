9dc8906e7aa05769c87761cf2c859bb64a9bcab7
SamuelSilva <sumersam2000@hotmail.com>
2026-08-02 19:25:37 -0600

    [REFCTOR] Adding param in app instance, supporting dynamic logger

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import Fastify, {
     type FastifyInstance,
+    type FastifyServerOptions,
 } from "fastify"
 
 import { registerPageRoutes } from "./routes/pages.routes.js"
 import { registerLinkFixtureRoutes } from "./routes/link-fixtures.routes.js"
 
-export function buildApp(): FastifyInstance {
-    const app = Fastify({
-        logger: true,
-    });
+export function buildApp(
+    options: FastifyServerOptions = {},
+): FastifyInstance {
+    const app = Fastify(options);
     
-    app.register(registerPageRoutes)
-    app.register(registerLinkFixtureRoutes)
+    app.register(registerPageRoutes);
+    app.register(registerLinkFixtureRoutes);
     
     return app;
 }
