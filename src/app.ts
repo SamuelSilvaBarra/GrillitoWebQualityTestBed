ad195f733fee91a515a0f8cfab1578d542ec3d66
SamuelSilva <sumersam2000@hotmail.com>
2026-08-08 13:39:13 -0600

    [ADD] Adding a valid svg image, adding prefix directory

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import Fastify, {
     type FastifyInstance,
     type FastifyServerOptions,
 } from "fastify"
 
+import fastifyStatic from "@fastify/static";
+
 import { registerPageRoutes } from "./routes/pages.routes.js"
 import { registerLinkFixtureRoutes } from "./routes/link-fixtures.routes.js"
+
+const currentFilename = fileURLToPath(import.meta.url);
+const currentDirectory = dirname(currentFilename);
+
 
 export function buildApp(
     options: FastifyServerOptions = {},
 ): FastifyInstance {
     const app = Fastify(options);
-    
+    app.register(fastifyStatic, {
+        root: join(currentDirectory, "../public"),
+        prefix: "/static/",
+    })
     app.register(registerPageRoutes);
     app.register(registerLinkFixtureRoutes);
     
