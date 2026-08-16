822749bf2210df5c004a58e8b975fc72aa729bdd
SamuelSilva <sumersam2000@hotmail.com>
2026-08-16 16:24:32 -0600

    [ADD] build scenario

---
+++ src/services/scenario.builder.ts
@@ -0,0 +1,17 @@
+import type { DemoScenario } from "../domain/scenario.js";
+import { defaultScenario } from "../domain/scenario.js"
+import type { ScenarioQuery } from "../schemas/scenario.query.schema.js"
+
+export function buildScenario( query: ScenarioQuery,): DemoScenario {
+    
+    if(Object.keys(query).length === 0){
+        return defaultScenario
+    }
+    return {
+        spelling: query.spelling ?? [],
+        links: query.links ?? [],
+        images: query.images ?? [],
+        videos: query.videos ?? [],
+        pdfs: query.pdfs ?? [],
+    }
+}
