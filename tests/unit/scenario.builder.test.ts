7803bc3202878f77f9d04306395df82981f82b18
SamuelSilva <sumersam2000@hotmail.com>
2026-08-16 16:25:32 -0600

    [ADD] Build scenario tests

---
+++ tests/unit/scenario.builder.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from "vitest";
+
+import { buildScenario } from "../../src/services/scenario.builder.js"
+import { defaultScenario } from "../../src/domain/scenario.js"
+
+
+describe("buildScenario", () => {
+    it("Test an empty scenario", () => {
+        const result = buildScenario({});
+
+        expect(result).toEqual(defaultScenario)
+    });
+    
+    it("Test one scenario", () => {
+        const result = buildScenario({
+            links: ["broken"],
+        });
+
+        expect(result.links).toEqual(["broken"])
+        expect(result.images).toEqual([])
+        expect(result.videos).toEqual([])
+        expect(result.pdfs).toEqual([])
+        expect(result.spelling).toEqual([])
+    });
+
+    it("Test two scenarios", () => {
+        const result = buildScenario({
+            images: ["working"],
+            videos: ["missing"]
+        });
+
+        expect(result.links).toEqual([])
+        expect(result.images).toEqual(["working"])
+        expect(result.videos).toEqual(["missing"])
+        expect(result.pdfs).toEqual([])
+        expect(result.spelling).toEqual([])
+    });
+    
+});
