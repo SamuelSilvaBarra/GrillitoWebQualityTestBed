ea890af9822b876bdb2f13c1d1b4943bfb36ce5c
SamuelSilva <sumersam2000@hotmail.com>
2026-08-16 16:56:33 -0600

    [ADD] Demo dinamyc features

--- src/routes/pages.routes.ts
+++ src/routes/pages.routes.ts
@@ -1,20 +1,40 @@
 import type { FastifyInstance } from "fastify";
+import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 
 import { presentScenario } from "../presenters/scenario.presenter.js";
 import { defaultScenario } from "../domain/scenario.js"
+import { ScenarioQuerySchema } from "../schemas/scenario.query.schema.js";
+import { buildScenario } from "../services/scenario.builder.js";
 
 
-export async function registerPageRoutes(
-    app: FastifyInstance,
-): Promise<void> {
+export const registerPageRoutes: FastifyPluginAsyncTypebox = 
+    async (app) => {
     
     app.get("/", async (_request, reply) => {
     
-        const scenarioView = presentScenario(defaultScenario);
-    
-        return reply.viewAsync("index.hbs", {
-         title: "Web Quality Testbed",
-         scenario: scenarioView,
-        });
-    });
+            const scenarioView = presentScenario(defaultScenario);
+        
+            return reply.viewAsync("index.hbs", {
+            title: "Web Quality Testbed",
+            scenario: scenarioView,
+            });
+        },
+    );
+        
+    app.get("/demo", 
+        {
+            schema: { 
+                querystring: ScenarioQuerySchema, 
+            }, 
+        }, 
+        async (request, reply) => {
+            const scenario = buildScenario(request.query);
+
+            const scenarioView = presentScenario(scenario);
+            return reply.viewAsync("index.hbs", {
+                title: "Web Quality Testbed",
+                scenario: scenarioView
+            });
+        },
+    );
 }
